d7adddd6f0128eefae8da1e246ba5fc14f57ff72
Siraaj Khandkar <siraaj@khandkar.net>
2025-03-05 15:26:24 -0500

    Increase logging structure and detail

--- src/cmd/dups.rs
+++ src/cmd/dups.rs
@@ -65,29 +65,41 @@ pub fn dups(
     enable_blake3_pass: bool,
     enable_sha2_512_pass: bool,
 ) -> anyhow::Result<()> {
-    let mut groups: Vec<Vec<Meta>> = vec![
-        data::find(root_path)?
-            .filter_map(Result::ok)
+    let mut groups: Vec<Vec<Meta>> = {
+        let span = tracing::debug_span!("find_files");
+        let _span_guard = span.enter();
+        let files: Vec<Meta> = data::find(root_path)?
+            .filter_map(|result| match result {
+                Err(error) => {
+                    tracing::error!(?error, "Failure while finding files.");
+                    None
+                }
+                Ok(m) => Some(m),
+            })
             .filter(Meta::is_regular_file)
             .filter(|Meta { size, .. }| *size > 0)
-            .collect(),
-    ];
+            .collect();
+        tracing::debug!(files = files.len(), "Found.");
+        vec![files]
+    };
 
     // TODO First pass should be group by (dev, inode) - which is 100%
     //      certainty, but is a special case in that even though it is
     //      most certain it is also cheapest.
 
-    for f in groupers(
+    for (span, f) in groupers(
         sample_size,
         chunk_size,
         enable_blake3_pass,
         enable_sha2_512_pass,
     ) {
+        let _span_guard = span.enter();
         groups = refine(&groups, f)?;
     }
 
     // TODO Optional last pass should be byte-by-bye comparisson.
 
+    tracing::debug!(groups = groups.len(), "Reporting.");
     for group in groups {
         // TODO Lister grouper outputs.
         for file in group {
@@ -106,6 +118,7 @@ fn refine<F>(
 where
     F: Send + Sync + Fn(&Meta) -> anyhow::Result<Vec<u8>>,
 {
+    tracing::debug!(groups = groups.len(), "Refining.");
     let grouper = Arc::new(grouper);
     let refined_groups: Vec<Vec<Meta>> = groups
         .par_iter()
@@ -151,52 +164,75 @@ fn groupers(
     chunk_size: usize,
     enable_blake3_pass: bool,
     enable_sha2_512_pass: bool,
-) -> Vec<Box<dyn Send + Sync + Fn(&Meta) -> anyhow::Result<Vec<u8>>>> {
-    let mut groupers: Vec<
+) -> Vec<(
+    tracing::Span,
+    Box<dyn Send + Sync + Fn(&Meta) -> anyhow::Result<Vec<u8>>>,
+)> {
+    let mut groupers: Vec<(
+        tracing::Span,
         Box<dyn Send + Sync + Fn(&Meta) -> anyhow::Result<Vec<u8>>>,
-    > = vec![
+    )> = vec![
         // 1: by size
-        Box::new(|m| Ok(m.size.to_le_bytes().to_vec())),
+        (
+            tracing::debug_span!("group_by_size"),
+            Box::new(|m| Ok(m.size.to_le_bytes().to_vec())),
+        ),
         // 2: by head bytes
-        Box::new(
-            move |Meta {
-                      path, size: total, ..
-                  }| {
-                let head_size =
-                    std::cmp::min(usize::try_from(*total)?, sample_size);
-                let mut file = fs::File::open(path)?;
-                let mut buf = vec![0u8; head_size];
-                file.read(&mut buf)?;
-                Ok(buf)
-            },
+        (
+            tracing::debug_span!("group_by_sample_head"),
+            Box::new(
+                move |Meta {
+                          path, size: total, ..
+                      }| {
+                    let head_size =
+                        std::cmp::min(usize::try_from(*total)?, sample_size);
+                    let mut file = fs::File::open(path)?;
+                    let mut buf = vec![0u8; head_size];
+                    file.read(&mut buf)?;
+                    Ok(buf)
+                },
+            ),
         ),
         // 3: by mid bytes
-        Box::new(
-            move |Meta {
-                      path, size: total, ..
-                  }| {
-                let start: u64 = total / u64::try_from(sample_size)? / 2;
-                let len: usize =
-                    std::cmp::min(usize::try_from(*total)?, sample_size);
-                let mut file = fs::File::open(path)?;
-                file.seek(SeekFrom::Start(start))?;
-                let mut buf = vec![0u8; len];
-                file.read(&mut buf)?;
-                Ok(buf)
-            },
+        (
+            tracing::debug_span!("group_by_sample_mid"),
+            Box::new(
+                move |Meta {
+                          path, size: total, ..
+                      }| {
+                    let start: u64 = total / u64::try_from(sample_size)? / 2;
+                    let len: usize =
+                        std::cmp::min(usize::try_from(*total)?, sample_size);
+                    let mut file = fs::File::open(path)?;
+                    file.seek(SeekFrom::Start(start))?;
+                    let mut buf = vec![0u8; len];
+                    file.read(&mut buf)?;
+                    Ok(buf)
+                },
+            ),
         ),
         // 4: by hash: xxh
-        Box::new(move |m| {
-            hash::xxh(&m.path, chunk_size).map(|h| h.to_le_bytes().to_vec())
-        }),
+        (
+            tracing::debug_span!("group_by_hash_xxh"),
+            Box::new(move |m| {
+                hash::xxh(&m.path, chunk_size)
+                    .map(|h| h.to_le_bytes().to_vec())
+            }),
+        ),
     ];
     if enable_blake3_pass {
         // 5: by hash: blake3
-        groupers.push(Box::new(move |m| hash::blake3(&m.path, chunk_size)));
+        groupers.push((
+            tracing::debug_span!("group_by_hash_blake3"),
+            Box::new(move |m| hash::blake3(&m.path, chunk_size)),
+        ));
     }
     if enable_sha2_512_pass {
         // 6: by hash: sha2-512
-        groupers.push(Box::new(move |m| hash::sha2_512(&m.path, chunk_size)));
+        groupers.push((
+            tracing::debug_span!("group_by_hash_sha2-512"),
+            Box::new(move |m| hash::sha2_512(&m.path, chunk_size)),
+        ));
     }
     groupers
 }
